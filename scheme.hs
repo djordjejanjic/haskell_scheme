32662df52fdc3b55acd1bebc5e5f315e269cd34e
djord <djord@DESKTOP-EA378QQ>
2020-08-05 13:31:41 +0200

    Citanje linije imena

--- scheme.hs
+++ scheme.hs
@@ -1,7 +1,13 @@
 module Main where
- import System.Environment
- 
- main :: IO ()
- main = do
-     args <- getArgs
-     putStrLn ("Hello, " ++ args !! 0)
+import System.Environment
+
+main :: IO ()
+main = do
+ -- Pitanje za ime i ispisivanje imena
+ putStrLn "Kako se zoves?"
+ name <- getLine
+ putStrLn ("Drago mi je da smo se upoznali " ++ name)
+
+-- Sabiranje dva broja
+-- args <- getArgs
+-- print ((read $ args!!0) + (read $ args!!1))
